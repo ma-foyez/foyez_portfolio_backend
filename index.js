4ae11d9a854bb709b79e912e023031494612ee3b
ma-foyeze <mafoyez.bd@gmail.comm>
2021-04-29 22:44:20 +0600

    blog list, create, edit, delete & others issue done

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({
 client.connect(err => {
     const portfolioCollection = client.db("Portfolio_Database").collection("PortfolioList");
     const skillsCollection = client.db("Portfolio_Database").collection("skillList");
+    const blogCollection = client.db("Portfolio_Database").collection("blogList");
     // perform actions on the collection object
 
     //post portfolio
@@ -77,7 +78,7 @@ client.connect(err => {
             })
     })
 
-    // load single event
+    // load single skill details
     app.get('/skill/:id', (req, res) => {
         skillsCollection.find({ _id: ObjectId(req.params.id) })
             .toArray((err, documents) => {
@@ -90,6 +91,49 @@ client.connect(err => {
         skillsCollection.updateOne({ _id: ObjectId(req.query.id) }, {
             $set: { technology: req.body.technology, expertise_level: req.body.expertise_level, tech_logo: req.body.tech_logo, tech_logoPreview: req.body.tech_logoPreview }
         })
+            .then(result => {
+                res.send(result)
+            })
+    })
+
+    //store blog
+    app.post('/add-blog', (req, res) => {
+        const blogData = req.body;
+        blogCollection.insertOne(blogData)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    //get blog list data
+    app.get('/blog-list', (req, res) => {
+        blogCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    // load single blog details
+    app.get('/blog/:id', (req, res) => {
+        blogCollection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
+    })
+
+    //update skills
+    app.patch('/update-blog', (req, res) => {
+        blogCollection.updateOne({ _id: ObjectId(req.query.id) }, {
+            $set: { title: req.body.title, blog_link: req.body.blog_link, blogBanner: req.body.blogBanner }
+        })
+            .then(result => {
+                res.send(result)
+            })
+    })
+
+    // delete skill from database
+    app.delete('/blog-delete', (req, res) => {
+        blogCollection.deleteOne({ _id: ObjectId(req.query.id) })
             .then(result => {
                 res.send(result)
             })
